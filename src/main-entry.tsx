1c29216ef2386410bf416bd34fde33540e980952
BUMBAIYA <amitchauhan263871@gmail.com>
2025-06-22 19:02:33 +0530

    learning element dragging

--- src/main-entry.tsx
+++ src/main-entry.tsx
@@ -1,10 +1,9 @@
+import { TestPiece } from "@/components/test-piece";
+
 export default function App() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center p-6">
-      <h1 className="text-4xl font-bold">Dynamic Board | Pragmatic DnD</h1>
-      <p className="text-lg">
-        Make dynamic boards like mixpanel using Pragmatic DnD and D3
-      </p>
+    <div className="p-6">
+      <TestPiece />
     </div>
   );
 }
