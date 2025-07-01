66aac71fd24d3ec0de02b507c44493d8673a8a24
BUMBAIYA <amitchauhan263871@gmail.com>
2025-07-01 23:58:13 +0530

    add draggable function to board card

--- src/components/dynamic-board-card.tsx
+++ src/components/dynamic-board-card.tsx
@@ -1,3 +1,9 @@
+import { useEffect, useRef } from "react";
+
+import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
+import invariant from "tiny-invariant";
+
 import type { DynamicBoardDNDCard } from "@/core/types";
 
 export interface DynamicBoardCardProps {
@@ -5,8 +11,28 @@ export interface DynamicBoardCardProps {
 }
 
 export function DynamicBoardCard({ card }: DynamicBoardCardProps) {
+  const cardElementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const element = cardElementRef.current;
+    invariant(element, "Card element not found");
+
+    return combine(
+      draggable({
+        element,
+        getInitialData: () => ({
+          id: card.id,
+          type: "card",
+        }),
+      }),
+    );
+  }, [card.id]);
+
   return (
-    <div className="flex min-h-60 flex-1 flex-col rounded-lg border border-gray-200 p-3">
+    <div
+      ref={cardElementRef}
+      className="flex min-h-60 flex-1 flex-col rounded-lg border border-gray-200 p-3"
+    >
       <div className="flex flex-col">{card.title}</div>
       <p>{JSON.stringify(card)}</p>
     </div>
