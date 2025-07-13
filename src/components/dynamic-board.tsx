fd7b28c4352ce8c5bf967a3f6f0bf2808d9dbdc1
BUMBAIYA <amitchauhan263871@gmail.com>
2025-07-13 19:00:27 +0530

    Consolidate imports in DynamicBoard. Introduce index file for core components to streamline exports.

--- src/components/dynamic-board.tsx
+++ src/components/dynamic-board.tsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 
-import { DynamicBoardWindowAutoScroll } from "@/core/components/dynamic-board-window-auto-scroll";
-import { DynamicBoardRow } from "@/core/components/dynamic-board-row";
-import { DynamicBoardCard } from "@/core/components/dynamic-board-card";
-import { DynamicBoardCardResizeHandle } from "@/core/components/dynamic-board-card-resize-handle";
-import { DynamicBoardRowHeightResizeHandle } from "@/core/components/dynamic-board-row-height-resize-handle";
+import {
+  DynamicBoardWindowAutoScroll,
+  DynamicBoardRow,
+  DynamicBoardCard,
+  DynamicBoardCardResizeHandle,
+  DynamicBoardRowHeightResizeHandle,
+} from "@/core/components";
 import { useDynamicBoard } from "@/core/hooks/useDynamicBoard";
 import { CardImplementation } from "@/components/card-implementation";
 import type { MockCardContent } from "@/types";
